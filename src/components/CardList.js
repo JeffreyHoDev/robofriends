e6385c17d07df18e07b7bf0f7bf341539142f11e
JeffreyHoDev <JeffreyHoDev@gmail.com>
2020-05-24 13:07:39 +0800

    Deploying web

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,9 +2,7 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({robots}) => {
-    if(true){
-        throw new Error('Noooooo!');
-    }
+
     const cardComponents = robots.map((user, i) => {
         return <Card key={i} id={robots[i].id} name= {robots[i].name} email={robots[i].email} />
         //You will get some error without key props, without it browser wont know what had been done to it
